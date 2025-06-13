d70a8e5ba51203dcf2f1aa677bed8dcbafe75f1b
Felipe Domingues <felipeeu@yahoo.com.br>
2025-06-13 15:30:48 -0300

    Add GET routes to change counter value

--- src/buzzlabs_phase_2/core.clj
+++ src/buzzlabs_phase_2/core.clj
@@ -1,7 +1,29 @@
 (ns buzzlabs-phase-2.core
-  (:gen-class))
+  (:gen-class)
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [compojure.core :refer [defroutes GET]]
+            [ring.middleware.cors :refer [wrap-cors]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def value (atom 0))
+
+(defn set-value 
+  "Sets the value of the atom to the provided number."
+  [number]
+  (reset! value number))
+
+(defroutes app-routes
+  (GET "/" [] (str @value))
+  (GET "/:number" [number] (set-value number)))
+
+(def app
+  (wrap-cors
+   app-routes
+   :access-control-allow-origin [#".*"]
+   :access-control-allow-methods [:get :post :put :delete :options]))
+
+(defn -main 
+  [] 
+  (run-jetty
+   app
+   {:port 3000})
+  )
