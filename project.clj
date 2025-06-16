60db4e47a7707908d02b371e848a945cda13e41b
Felipe Domingues <felipeeu@yahoo.com.br>
2025-06-16 05:31:04 -0300

    connect to datomic local db

--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring "1.14.1"]
+                 [ring "1.9.6"]
+                 [ring/ring-jetty-adapter "1.9.6"]
                  [compojure "1.7.1"]
-                 [ring-cors "0.1.13"]
+                 [ring-cors "0.1.13"] 
+                 [com.datomic/local "1.0.291"]
+                 [com.datomic/client "1.0.139"]
                  ]
   :main ^:skip-aot buzzlabs-phase-2.core
   :target-path "target/%s"
--- src/buzzlabs_phase_2/core.clj
+++ src/buzzlabs_phase_2/core.clj
@@ -2,18 +2,42 @@
   (:gen-class)
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer [defroutes GET]]
-            [ring.middleware.cors :refer [wrap-cors]]))
+            [ring.middleware.cors :refer [wrap-cors]]
+            [datomic.client.api :as d]
+            ))
 
-(def value (atom 0))
+(def client (d/client {:server-type :datomic-local
+                       :system "datomic-samples"}))
+(def db-name "counter-db")
 
-(defn set-value 
-  "Sets the value of the atom to the provided number."
+(def conn (d/connect client {:db-name db-name}))
+
+(defn transact-incremented-value
+  "transacts the number into datomic database.
+   the number is expected to be an integer."
   [number]
-  (reset! value number))
+  (println "Transacting value:" number)
+  (d/transact conn {:tx-data [{:counter/id :counter
+                               :counter/value number}]})
+  )
+
+(defn get-counter-value
+  "get the current value from the counter database.
+   returns the value as a string."
+  []
+  (let [db (d/db conn)
+        current-value (ffirst (d/q '[:find ?value
+                                     :where
+                                     [?e :counter/value ?value]
+                                     [?e :counter/id :counter]] db))]
+    
+    (println "Current value from datomic:" current-value)
+    (str current-value)
+    ))
 
 (defroutes app-routes
-  (GET "/" [] (str @value))
-  (GET "/:number" [number] (set-value number)))
+  (GET "/" [] (get-counter-value))
+  (GET "/:number" [number] (transact-incremented-value (Integer/parseInt number))))
 
 (def app
   (wrap-cors
